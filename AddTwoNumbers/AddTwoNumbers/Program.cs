96648f8c637f3eadc26cc44013c443620fc1c01d
TAFE-CSharpGroup <60644918+TAFE-CSharpGroup@users.noreply.github.com>
2020-02-04 18:35:17 +1000

    Add files via upload

---
+++ AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt for the 1st number
+            Console.Write("Please enter the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            // Prompt for the 2nd number
+            Console.Write("Please enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            // Calculate the sum
+            int sum = firstNumber + secondNumber;
+
+            // Display results
+            Console.Write($"The sum is {sum}.");
+
+            // Pause the program
+            Console.ReadLine();
+
+        }
+    }
+}
