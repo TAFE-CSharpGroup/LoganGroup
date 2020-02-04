e81260927eef30095f28f4648a944771a8ffe625
TAFE-CSharpGroup <60644918+TAFE-CSharpGroup@users.noreply.github.com>
2020-02-04 18:38:43 +1000

    Add files via upload

---
+++ SecondsToMinutes/SecondsToMinutes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondsToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt for the time in seconds
+            Console.Write("Please enter the time in seconds: ");
+            int seconds = int.Parse(Console.ReadLine());
+
+            // Convert to minutes
+            double minutes = seconds / 60.0;
+
+            // Display the results
+            Console.WriteLine($"The answer is {minutes} minutes.");
+
+            // Pause the program
+            Console.ReadLine();
+
+        }
+    }
+}
